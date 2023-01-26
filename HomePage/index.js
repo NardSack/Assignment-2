e871cef68d0389b5b4546e5425fcfc393047e1b0
NardSack <S10242618@connect.np.edu.sg>
2023-01-26 22:58:17 +0800

    java script for home html

--- HomePage/index.js
+++ HomePage/index.js
@@ -1,3 +1,24 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://assign2project-142c.restdb.io/rest/itemsdetail",
+  "method": "GET",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "63d1f6cda95709597409cf9e",
+    "cache-control": "no-cache"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+  for (var i = 0; i < response.length; i++) {
+    let description = response[i].Descriptionofproduct;
+    console.log(description)
+  }
+});
+
+
 var myIndex = 0;
   carousel();
   
@@ -11,4 +32,6 @@ var myIndex = 0;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 6000); // Change image every 6 seconds
-  }
+  };
+
+
