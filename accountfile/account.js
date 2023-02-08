a36d9bdf0ff5952d27c557d5b41678ec9e46a212
NardSack <S10242618@connect.np.edu.sg>
2023-02-09 01:58:35 +0800

    account find

--- accountfile/account.js
+++ accountfile/account.js
@@ -1,2 +1,4 @@
 var cart=localStorage.getItem("cartJSON")
 console.log(localStorage.getItem("Name"))
+var names = localStorage.getItem("Name")
+document.querySelector("useraccount").querySelector("h2").innerHTML = "account "+names;
