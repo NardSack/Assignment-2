3f849eaccde82243867416ddbc01a4935a586763
Ng Lyna <s10242463@connect.np.edu.sg>
2023-02-08 18:35:46 +0800

    review + mobile view for idex

--- HomePage/index.js
+++ HomePage/index.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
 
 var productpage=document.getElementById("productpage")
 
-productpage.style.top="%"
+productpage.style.top="-110%"
 // $.when(data).done(function(){})
 
   document.getElementById("content").addEventListener("click", function(event){
@@ -80,52 +80,14 @@ productpage.style.top="%"
     list.forEach(function(item) {
       if (item._id==targetid)
       {
-        productpage.querySelector("picture").innerHTML=`<img src="${item.PictureLink}"></img>`
+        productpage.querySelector("picture").innerHTML=`<img src="${item.PictureLink}" class="clickpic"></img>`
         productpage.querySelector("description").innerHTML=`<h2>${item.NameofProduct}</h2><h3>Type ${item.Gender} ${item.TypeofProduct}</h3></br><p>${item.Descriptionofproduct}</p></br><p>${item.PriceofProduct}</p>`
         
       }
-      
-      // var i=data.responseJSON
-      // console.log(i)
-      // foreach()
-
-      $("#submit").on("click", function (e) {
-        //prevent default action of the button 
-        e.preventDefault();
-      // let review = $("#review").value;
-      var review = document.getElementById("review").value;
-      
-      var jsondata = {
-        "reviewcomment": review,
-        "itemid":targetid,
-        "userid":1}; 
-        
-
-      var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://assign2project-142c.restdb.io/rest/review/(ObjectID)",
-        "method": "PUT",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": "63d1f6cda95709597409cf9e",
-          "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata)
-      }
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-      
-       
-       
-      })      
-    });
 
     });
     if (productpage.style.top=="-110%")
-    {productpage.style.top="0"
+    {productpage.style.top="-0"
   
   }
 
@@ -134,7 +96,6 @@ productpage.style.top="%"
 function hiding(){
   productpage.style.top="-110%"
 }
-
 
 // function show(id){
 //     let id = id
@@ -145,4 +106,55 @@ function hiding(){
 // function hide()
 // {productpage.style.top="-100%"
 // }
-// -------------------
+// ---------Adding Reviews----------
+
+getContacts();
+function saveData() {
+  // Get the input values
+  var comments = document.getElementById("comments").value;
+  document.getElementById("outputField").innerHTML = comments;
+//   var email = document.getElementById("email").value;
+
+  // Prepare the data to be sent to RESTdb
+  var data = {
+    userid:100,
+    comments: comments,
+    itemid:101,
+    // email: email
+  };
+
+  // Send the data to RESTdb
+  fetch("https://signlog-8d3d.restdb.io/rest/review", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "63e129ac3bc6b255ed0c470f"
+    },
+    body: JSON.stringify(data)
+  })
+  
+
+  
+}
+
+function getContacts(limit = 10, all = true) {
+
+    fetch("https://signlog-8d3d.restdb.io/rest/review", {
+headers: {
+"Content-Type": "application/json; charset=utf-8",
+"x-apikey": "63e129ac3bc6b255ed0c470f"
+}
+})
+.then(response => response.json())
+.then(data => {
+let tableBody = document.getElementById("dataTableBody");
+data.forEach(item => {
+  let tableRow = document.createElement("tr");
+  tableRow.innerHTML = `<td>${item.comments}</td>`;
+  tableBody.appendChild(tableRow);
+});
+});
+
+
+
+}
